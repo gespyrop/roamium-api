9bc52329fe8a7df6146050b5edcdc8e6839dfd36
gespyrop <giwrgosspyropoulos@gmail.com>
2021-04-22 02:31:10 +0300

    Confihured django with PostgreSQL

--- README.md
+++ README.md
@@ -7,10 +7,3 @@ The official REST API that powers the Roamium application.
 
 ### Entity Relationship Diagram
 ![ERD](docs/diagrams/ERD.png)
-
-### Use Cases
-- ##### Admin Panel
-![Admin Panel Use Cases](docs/diagrams/Admin_Panel_Use_Cases.png)
-
-- ##### Mobile App
-![Mobile App Use Cases](docs/diagrams/Mobile_App_Use_Cases.png)
--- roamium/roamium/settings.py
+++ roamium/roamium/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'roamium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD')
     }
 }
 
