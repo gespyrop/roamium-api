a1071afced9eb72d6d7f84ea3c7b2ca5ed7a7d9c
gespyrop <giwrgosspyropoulos@gmail.com>
2021-04-27 22:50:12 +0300

    Places app and GeoDjango

---
+++ roamium/places/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Place
+
+@admin.register(Place)
+class PlaceAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location', 'time')
+    default_lat = 37.9838
+    default_lon = 23.7275
+    default_zoom = 3
