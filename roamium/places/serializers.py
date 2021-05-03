d3988cc6619a2868dad5ca745984ec79161407cd
gespyrop <giwrgosspyropoulos@gmail.com>
2021-05-03 14:09:52 +0300

    Added test for  place category list

--- roamium/places/serializers.py
+++ roamium/places/serializers.py
@@ -1,9 +1,16 @@
 from drf_extra_fields.geo_fields import PointField
 from rest_framework import serializers
-from .models import Place
+from .models import Place, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    categories = serializers.ListSerializer(read_only=True, child=serializers.CharField())
     location = PointField()
     class Meta:
         model = Place
