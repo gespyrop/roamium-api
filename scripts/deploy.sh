fabdd4885175457c437ba0fb13b1e4c0dfa6c57b
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-25 20:26:38 +0200

    Fixed production deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ scp -i /tmp/deploy_rsa env/production.env george@roamium.software:$ROAMIUM_DIREC
 ssh -i /tmp/deploy_rsa george@roamium.software "echo $CR_PAT | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin"
 ssh -i /tmp/deploy_rsa george@roamium.software cd $ROAMIUM_DIRECTORY && \
     export POSTGRES_PASS=$(openssl rand -base64 32) && \
-    export SECRET_KEY=$(openssl rand -base64 32) && \
+    export "SECRET_KEY=$(openssl rand -base64 32) && \
     docker-compose --env-file $ROAMIUM_DIRECTORY/production.env down && \
     docker-compose --env-file $ROAMIUM_DIRECTORY/production.env pull && \
-    docker-compose --env-file $ROAMIUM_DIRECTORY/production.env up -d
+    docker-compose --env-file $ROAMIUM_DIRECTORY/production.env up -d"
 ssh -i /tmp/deploy_rsa george@roamium.software docker logout ghcr.io
