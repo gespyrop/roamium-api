a8e81b1fd0062d45cac54ed389bb1254623a3f49
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-25 18:13:12 +0200

    Second deploy attempt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-scp -i /tmp/deploy_rsa ../docker-compose.yml george@roamium.software:
+scp -i /tmp/deploy_rsa docker-compose.yml george@roamium.software:
+ssh -i /tmp/deploy_rsa george@roamium.software "echo $CR_PAT | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin"
 ssh -i /tmp/deploy_rsa george@roamium.software docker-compose down && docker-compose pull && docker-compose up -d
+ssh -i /tmp/deploy_rsa george@roamium.software docker logout ghcr.io
