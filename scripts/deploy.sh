229c590693ff937137966351ad17c1807f4ea963
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-25 19:40:44 +0200

    Copy env/production.env recursively

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 ROAMIUM_DIRECTORY=/home/george/roamium
 
 scp -i /tmp/deploy_rsa docker-compose.yml george@roamium.software:$ROAMIUM_DIRECTORY
-scp -i /tmp/deploy_rsa env/production.env george@roamium.software:$ROAMIUM_DIRECTORY
+scp -i /tmp/deploy_rsa -r env/production.env george@roamium.software:$ROAMIUM_DIRECTORY
 ssh -i /tmp/deploy_rsa george@roamium.software "echo $CR_PAT | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin"
 ssh -i /tmp/deploy_rsa george@roamium.software cd $ROAMIUM_DIRECTORY && \
     export POSTGRES_PASS=$(openssl rand -base64 32) && \
