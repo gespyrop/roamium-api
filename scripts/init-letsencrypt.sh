0e2ae7c75a296473887971a8d13f6a5cfcc31c8d
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-26 11:46:01 +0200

    Added acme-challenge location in nginx.conf

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -7,10 +7,8 @@ email="giwrgosspyropoulos@gmail.com" # Adding a valid address is strongly recomm
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
+  echo "Existing data found for $domains. Skipping..."
+  exit
 fi
 
 
@@ -67,6 +65,7 @@ docker compose run --rm --entrypoint "\
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
+    --non-interactive \
     --agree-tos \
     --force-renewal" certbot
 echo
