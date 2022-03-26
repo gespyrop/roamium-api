b01d24fef10c50933bd4b3aa8b322c79d493f93f
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-26 02:22:04 +0200

    Setup nginx for https

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,12 @@ ROAMIUM_DIRECTORY=/home/george/roamium
 
 scp -i /tmp/deploy_rsa docker-compose.yml george@roamium.software:$ROAMIUM_DIRECTORY
 scp -i /tmp/deploy_rsa env/production.env george@roamium.software:$ROAMIUM_DIRECTORY/.env
+scp -i /tmp/deploy_rsa scripts/init-letsencrypt.sh george@roamium.software:$ROAMIUM_DIRECTORY
 ssh -i /tmp/deploy_rsa george@roamium.software "echo $CR_PAT | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin"
 ssh -i /tmp/deploy_rsa george@roamium.software "cd $ROAMIUM_DIRECTORY && \
+    chmod +x init-letsencrypt.sh && \
+    ./init-letsencrypt.sh && \
+    rm init-letsencrypt.sh && \
     export POSTGRES_PASS=$(openssl rand -base64 32) && \
     export SECRET_KEY=$(openssl rand -base64 32) && \
     docker compose down && \
