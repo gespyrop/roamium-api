5c5d2c968e2b354fd3adb08dac00a72abf13df68
gespyrop <giwrgosspyropoulos@gmail.com>
2022-03-26 11:55:48 +0200

    Define secrets before running init-letsencrypt.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,12 @@ scp -i /tmp/deploy_rsa env/production.env george@roamium.software:$ROAMIUM_DIREC
 scp -i /tmp/deploy_rsa scripts/init-letsencrypt.sh george@roamium.software:$ROAMIUM_DIRECTORY
 ssh -i /tmp/deploy_rsa george@roamium.software "echo $CR_PAT | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin"
 ssh -i /tmp/deploy_rsa george@roamium.software "cd $ROAMIUM_DIRECTORY && \
+    docker compose down && \
+    docker compose pull && \
+    export POSTGRES_PASS=$(openssl rand -base64 32) && \
+    export SECRET_KEY=$(openssl rand -base64 32) && \
     chmod +x init-letsencrypt.sh && \
     ./init-letsencrypt.sh && \
     rm init-letsencrypt.sh && \
-    export POSTGRES_PASS=$(openssl rand -base64 32) && \
-    export SECRET_KEY=$(openssl rand -base64 32) && \
-    docker compose down && \
-    docker compose pull && \
     docker compose up -d"
 ssh -i /tmp/deploy_rsa george@roamium.software docker logout ghcr.io
