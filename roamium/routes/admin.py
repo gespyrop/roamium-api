fc2a4dba16383b4f46a6b856536ed44bc5a0dff9
gespyrop <giwrgosspyropoulos@gmail.com>
2022-09-01 21:20:37 +0300

    Added Review model to admin panel

--- roamium/routes/admin.py
+++ roamium/routes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Route, Visit
+from .models import Route, Visit, Review
 
 admin.site.register(Route)
 admin.site.register(Visit)
+admin.site.register(Review)
